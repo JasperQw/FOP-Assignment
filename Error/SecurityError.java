67a6dcad2f0a89b4e4f1a549a1134db2f15715b6
ChingYingPua <121926943+ChingYingPua@users.noreply.github.com>
2023-01-06 13:51:45 +0800

    Add files via upload

---
+++ Error/SecurityError.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+public class SecurityError {
+    public static void main(String[] args) {    
+        String keyword = "SECURITY";
+        try {
+            Scanner sc = new Scanner(new FileInputStream("C:\\Users\\ProUser\\OneDrive\\Documents\\NetBeansProjects\\test\\Error.txt"));
+            PrintWriter pw = new PrintWriter(new FileOutputStream("SecurityError.txt"));
+          while(sc.hasNextLine()) {
+                String read = sc.nextLine();
+                    if(read.toLowerCase().indexOf(keyword.toLowerCase()) >= 0)
+                        pw.println(read);
+               
+            }          
+            sc.close();
+            pw.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+}
