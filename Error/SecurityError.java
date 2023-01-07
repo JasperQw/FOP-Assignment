1189473531eafb98a0d3bf6eafc7b8b1cb34a6de
ChingYingPua <121926943+ChingYingPua@users.noreply.github.com>
2023-01-07 22:28:54 +0800

    Update SecurityError.java

--- Error/SecurityError.java
+++ Error/SecurityError.java
@@ -2,20 +2,70 @@ import java.io.*;
 import java.util.Scanner;
 public class SecurityError {
     public static void main(String[] args) {    
-        String keyword = "SECURITY";
         try {
-            Scanner sc = new Scanner(new FileInputStream("C:\\Users\\ProUser\\OneDrive\\Documents\\NetBeansProjects\\test\\Error.txt"));
+            String keyword = "SECURITY";
+            int totalError = 0;
+            Scanner sc = new Scanner(new FileInputStream("C:\\Users\\ProUser\\Downloads\\extracted_log"));
             PrintWriter pw = new PrintWriter(new FileOutputStream("SecurityError.txt"));
-          while(sc.hasNextLine()) {
+            while(sc.hasNextLine()) {
                 String read = sc.nextLine();
-                    if(read.toLowerCase().indexOf(keyword.toLowerCase()) >= 0)
+                    if(read.toLowerCase().indexOf(keyword.toLowerCase()) >= 0){
                         pw.println(read);
-               
+                        totalError += 1;
+                    }
             }          
+            System.out.println("The total security error in this log file is: " + totalError);
             sc.close();
             pw.close();
         } catch (FileNotFoundException ex) {
+            System.out.println("Error: " + ex); 
+        }
+        try { 
+            char ch = ' ';         
+           
+            Scanner sc = new Scanner(new FileInputStream("SecurityError.txt"));
+            int i = 0;
+            int Error6 = 0,Error7 = 0,Error8 = 0,Error9 = 0,Error10 = 0,Error11 = 0,Error12 = 0;
+            while(sc.hasNextLine()){
+                String read = sc.nextLine();
+                ch = read.charAt(7);
+                if(ch == '6'){
+                        Error6 += 1; 
+                }
+                else if(ch == '7'){
+                        Error7 += 1; 
+                }
+                else if(ch== '8'){
+                        Error8 += 1; 
+                }
+                else if(ch == '9'){
+                        Error9 += 1; 
+                }
+                else if(ch == '0'){
+                        Error10 += 1; 
+                }
+                else if(ch == '1'){
+                        Error11 += 1; 
+                }
+                else if(ch == '2'){
+                        Error12 += 1; 
+                }            
+            } 
+                System.out.println("The total security error occur in June: " + Error6);
+                System.out.println("The total security error occur in July: " + Error7);
+                System.out.println("The total security error occur in August: " + Error8);
+                System.out.println("The total security error occur in September: " + Error9);
+                System.out.println("The total security error occur in October: " + Error10);
+                System.out.println("The total security error occur in November: " + Error11);
+                System.out.println("The total security error occur in December: " + Error12);
+
+                AnalysisError obj = new AnalysisError(Error6,Error7,Error8,Error9,Error10,Error11,Error12);
+                obj.Max();      
+                obj.Min();
+               
+        } catch (FileNotFoundException ex) {
             System.out.println("Error: " + ex);
         }
+        
     }
 }
