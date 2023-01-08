4c952975dcb299a7da851e1a493daf973a13ccef
Kalok <116157954+yeepkalok0202@users.noreply.github.com>
2023-01-08 23:37:08 +0800

    Update AnalysisError.java

--- Error/AnalysisError.java
+++ Error/AnalysisError.java
@@ -11,48 +11,48 @@ public class AnalysisError{
     }
     public void Max(){
           if(Error6 > Error7 && Error6 > Error8 && Error6 > Error9 && Error6 > Error10 && Error6 > Error11 && Error6 > Error12){
-                    System.out.println("The month that have the most error: June ");
+                    System.out.println("The month that has the most errors: June ");
                 }
                 else if(Error7 > Error6 && Error7 > Error8 && Error7 > Error9 && Error7 > Error10 && Error7 > Error11 && Error7 > Error12){
-                    System.out.println("The month that have the most error: July");
+                    System.out.println("The month that has the most errors: July");
                 }
                 else if(Error8 > Error6 && Error8 > Error7 && Error8 > Error9 && Error8 > Error10 && Error8 > Error11 && Error8 > Error12){
-                    System.out.println("The month that have the most error: August");
+                    System.out.println("The month that has the most errors: August");
                 }
                 else if(Error9 > Error6 && Error9 > Error7 && Error9 > Error8 && Error9 > Error10 && Error9 > Error11 && Error9 > Error12){
-                    System.out.println("The month that have the most error: September");
+                    System.out.println("The month that has the most errors: September");
                 }
                 else if(Error10 > Error6 && Error10 > Error7 && Error10 > Error8 && Error10 > Error9 && Error10 > Error11 && Error10 > Error12){
-                    System.out.println("The month that have the most error: October");
+                    System.out.println("The month that has the most errors: October");
                 }
                 else if(Error11 > Error6 && Error11 > Error7 && Error11 > Error8 && Error11 > Error9 && Error11 > Error10 && Error11 > Error12){
-                    System.out.println("The month that have the most error: November");
+                    System.out.println("The month that has the most errors: November");
                 }
                 else{
-                    System.out.println("The month that have the most error: December");
+                    System.out.println("The month that has the most errors: December");
                 }
     }
     public void Min(){
           if(Error6 < Error7 && Error6 < Error8 && Error6 < Error9 && Error6 < Error10 && Error6 < Error11 && Error6 < Error12){
-                    System.out.println("The month that have the least error: June ");
+                    System.out.println("The month that has the least error: June ");
                 }
                 else if(Error7 < Error6 && Error7 < Error8 && Error7 < Error9 && Error7 < Error10 && Error7 < Error11 && Error7 < Error12){
-                    System.out.println("The month that have the least error: July");
+                    System.out.println("The month that has the least error: July");
                 }
                 else if(Error8 < Error6 && Error8 < Error7 && Error8 < Error9 && Error8 < Error10 && Error8 < Error11 && Error8 < Error12){
-                    System.out.println("The month that have the least error: August");
+                    System.out.println("The month that has the least error: August");
                 }
                 else if(Error9 < Error6 && Error9 < Error7 && Error9 < Error8 && Error9 < Error10 && Error9 < Error11 && Error9 < Error12){
-                    System.out.println("The month that have the least error: September");
+                    System.out.println("The month that has the least error: September");
                 }
                 else if(Error10 < Error6 && Error10 < Error7 && Error10 < Error8 && Error10 < Error9 && Error10 < Error11 && Error10 < Error12){
-                    System.out.println("The month that have the least error: October");
+                    System.out.println("The month that has the least error: October");
                 }
                 else if(Error11 < Error6 && Error11 < Error7 && Error11 < Error8 && Error11 < Error9 && Error11 < Error10 && Error11 < Error12){
-                    System.out.println("The month that have the least error: November");
+                    System.out.println("The month that has the least error: November");
                 }
                 else{
-                    System.out.println("The month that have the least error: December");
+                    System.out.println("The month that has the least error: December");
                 }
     }
     public void Average(){
