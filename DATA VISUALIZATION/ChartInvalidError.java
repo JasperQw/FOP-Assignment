b02a5dc974c3ea979bc25f4d5ac5a6bf6103b8cf
ChingYingPua <121926943+ChingYingPua@users.noreply.github.com>
2023-01-14 15:08:29 +0800

    Add files via upload

---
+++ DATA VISUALIZATION/ChartInvalidError.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.stage.Stage;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+public class ChartInvalidError extends Application{
+    
+   public void start(Stage stage) {                 
+      CategoryAxis xAxis = new CategoryAxis();  
+      xAxis.setCategories(FXCollections.<String>
+      observableArrayList(Arrays.asList("June", "July", "August", "September","October","November","December")));
+      xAxis.setLabel("Month");
+       
+      NumberAxis yAxis = new NumberAxis();
+      yAxis.setLabel("Number of error");
+     
+      //Creating the Bar chart
+      BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis); 
+      barChart.setTitle("Number of Invalid qos Errors According Month");
+        
+      //Prepare XYChart.Series objects by setting data       
+      XYChart.Series<String, Number> series1 = new XYChart.Series<>();
+      series1.setName("Error");
+      series1.getData().add(new XYChart.Data<>("June", 42));
+      series1.getData().add(new XYChart.Data<>("July", 0));
+      series1.getData().add(new XYChart.Data<>("August", 59));
+      series1.getData().add(new XYChart.Data<>("September", 12));
+      series1.getData().add(new XYChart.Data<>("October", 5));
+      series1.getData().add(new XYChart.Data<>("November", 32));
+      series1.getData().add(new XYChart.Data<>("December", 2));
+      
+      barChart.getData().addAll(series1);
+      Group root = new Group(barChart);
+      Scene scene = new Scene(root, 600, 400);
+      stage.setTitle("Bar Chart");
+      stage.setScene(scene);
+      stage.show();        
+   }
+   public static void main(String args[]){
+      launch(args);
+   }
+}
+
