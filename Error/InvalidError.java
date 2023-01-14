e4792946fa8851b2440ba98f23aba13901936f57
ChingYingPua <121926943+ChingYingPua@users.noreply.github.com>
2023-01-14 15:11:49 +0800

    Update InvalidError.java

--- Error/InvalidError.java
+++ Error/InvalidError.java
@@ -2,15 +2,15 @@ import Data.Table;
 import java.util.Scanner;
 import java.io.*;
 public class InvalidError {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String keywords1 = "Invalid";
         String keywords2 = "qos";
         String keywords3 = "slurm";
         String keywords4 = "submit";
         int totalError = 0;
-        try {
-            Scanner sc = new Scanner(new FileInputStream("C:\\Users\\ProUser\\Downloads\\extracted_log"));
-            PrintWriter pw = new PrintWriter(new FileOutputStream("InvalidErrorNew.txt"));
+        //read the log file and write the line that contain the keyword into a txt.file
+        Scanner sc = new Scanner(new FileInputStream("C:\\Users\\ProUser\\Downloads\\extracted_log"));
+        PrintWriter pw = new PrintWriter(new FileOutputStream("InvalidErrorNew.txt"));
             while(sc.hasNextLine()){
                 String read = sc.nextLine();
                     if(read.indexOf(keywords1) >= 0 && read.indexOf(keywords2) >= 0 && read.indexOf(keywords3) >= 0 && read.indexOf(keywords4) >= 0){
@@ -21,17 +21,13 @@ public class InvalidError {
             sc.close();
             pw.close();
             System.out.println("The total invalid qos error in this log file is: " + totalError);
-        } catch (FileNotFoundException ex) {
-            System.out.println("Error: " + ex);
-        }
-        try { 
+
+            //find the error occur in each month
             char ch = ' ';         
-           
-            Scanner sc = new Scanner(new FileInputStream("InvalidErrorNew.txt"));
-            int i = 0;
+            Scanner input = new Scanner(new FileInputStream("InvalidErrorNew.txt"));
             int Error6 = 0,Error7 = 0,Error8 = 0,Error9 = 0,Error10 = 0,Error11 = 0,Error12 = 0;
-            while(sc.hasNextLine()){
-                String read = sc.nextLine();
+            while(input.hasNextLine()){
+                String read = input.nextLine();
                 ch = read.charAt(7);
                 if(ch == '6'){
                         Error6 += 1; 
@@ -54,7 +50,7 @@ public class InvalidError {
                 else if(ch == '2'){
                         Error12 += 1; 
                 }            
-            } 
+            }
                 System.out.println("The total invalid qos error occur in June: " + Error6);
                 System.out.println("The total invalid qos occur in July: " + Error7);
                 System.out.println("The total invalid qos occur in August: " + Error8);
@@ -62,10 +58,14 @@ public class InvalidError {
                 System.out.println("The total invalid qos occur in October: " + Error10);
                 System.out.println("The total invalid qos occur in November: " + Error11);
                 System.out.println("The total invalid qos occur in December: " + Error12);
+                
+                //Analysis data
                 AnalysisError obj = new AnalysisError(Error6,Error7,Error8,Error9,Error10,Error11,Error12);
-                obj.Max();      
-                obj.Min();
-                obj.Average();
+                System.out.println("Month with the most invalid qos error: "+ obj.Max());   
+                System.out.println("Month with the least invalid qos error: "+ obj.Min());  
+                System.out.printf("Average invalid qos error in each month: %.2f ", obj.Average(totalError));
+                
+                //tabulate the month and number of error in each month
                 String title = "Invalid Qos Error";
                     String [] columns = {"Month", "Number of Error"};
                     Object data[][] = {{"June", Error6 },
@@ -76,12 +76,6 @@ public class InvalidError {
                                        {"November", Error11},
                                        {"December", Error12}
                                        };
-                    Table tb = new Table(title, columns,data);
-        } catch (FileNotFoundException ex) {
-            System.out.println("Error: " + ex);
-        }
-       
-       
+                    Table tb = new Table(title, columns,data);    
     }
-   
 }
