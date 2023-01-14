4d4a57ef6c4d89a0da1353b0d5993d2273120d0d
Kalok <116157954+yeepkalok0202@users.noreply.github.com>
2023-01-14 17:07:13 +0800

    Add files via upload

---
+++ Kalok/testerclassfortimecalculator.java
@@ -0,0 +1,13 @@
+public class testerclassfortimecalculator extends timecalculator{
+    public static void main(String[] args)throws Exception{
+        String start="[2022-06-04T11:53:50.187]";
+        String end="[2022-06-04T14:45:08.571]";
+
+        //findforconsole(start,end);
+
+
+
+        
+        formaltimeconverter(1000); //receive arguement in form of milliseconds
+    }
+}
