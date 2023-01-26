d90eb043fc686898aab17e30dbf6083f58bd32fa
Michelle Hu Ynh Tin <116173980+michelle264@users.noreply.github.com>
2023-01-27 01:17:24 +0800

    Add files via upload

---
+++ DATA VISUALIZATION/Nodes umhpc error/Table.java
@@ -0,0 +1,18 @@
+package nodes_umphc;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class Table {
+    JFrame fr;
+        JTable jt;
+        public Table(String title,String[]columns,Object[][]data){
+            fr = new JFrame(title);
+            jt = new JTable(data,columns);
+            jt.setBounds(30, 40, 200, 300);
+            JScrollPane jsp = new JScrollPane(jt);
+            fr.add(jsp);
+            fr.setSize(500, 200);
+            fr.setVisible(true);
+        }
+}
